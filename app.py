e1f701c6e8e5718820fa47fd485a7babd41257bb
kvllinz <54149346+kvllinz@users.noreply.github.com>
2021-11-03 14:48:34 -0400

    Setup LoginScreen

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from typing import Text
 import flask
+from flask.helpers import url_for
 from flask_login.utils import login_required
 import os
 import json
@@ -11,8 +13,16 @@ app = flask.Flask(__name__, static_folder='./build/static')
 # why this is necessary.
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
 
+app.secret_key = os.urandom(9)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    """ This is a catch all that is required for react-router """
+    return flask.render_template('index.html')
+
 @bp.route('/index')
-@login_required
+# @login_required
 def index():
     # TODO: insert the data fetched by your app main page here as a JSON
     DATA = {"your": "data here"}
@@ -24,35 +34,41 @@ def index():
 
 app.register_blueprint(bp)
 
-@app.route('/signup')
-def signup():
-	...
+# @app.route('/signup')
+# def signup():
+# 	...
 
-@app.route('/signup', methods=["POST"])
-def signup_post():
-	...
+# @app.route('/signup', methods=["POST"])
+# def signup_post():
+# 	...
 
-@app.route('/login')
-def login():
-    ...
+# @app.route('/login')
+# def login():
+#     ...
 
 @app.route('/login', methods=["POST"])
 def login_post():
-	...
 
-@app.route('/save', methods=["POST"])
-def save():
-    ...
+    username= flask.request.json.get("username")
+    password = flask.request.json.get("password")
 
+    if len(username) == 0 and len(password) == 0:
+        flask.flash("Enter valid Username. Please try again")
+    
+    if username == "Collins" and password == "Amadi":
+        return flask.jsonify({"loginResponse": "Ok"})
+	
 
-@app.route('/')
-def main():
-	...
+# @app.route('/save', methods=["POST"])
+# def save():
+#     ...
+
 
 
 
 
 app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8081)),
+    # host=os.getenv('IP', '0.0.0.0'),
+    # port=int(os.getenv('PORT', 8081)),
+    debug=True
 )
---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home =()=>{
+    return (
+        <p> Hello Collins</p>
+    )
+}
+
+export default Home;
