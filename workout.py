367d517c8a446466cfe88505e90717fa2f3f439e
Muhibhotak <72638193+Muhibhotak@users.noreply.github.com>
2021-11-16 12:09:09 -0500

    deleted comments and unnecessary lines of code

--- workout.py
+++ workout.py
@@ -6,7 +6,6 @@ app = flask.Flask(__name__)
 
 url = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_DATABASE_URI'] = url
-# "postgresql://jfcnsbxgppwgdq:14793a5b54f0b5a25a6359a95c07a71b0a42b076q073b5c556b7ef739a9076fba@ec2-3-230-149-158.compute-1.amazonaws.com:5432/d4fptrh111bdmj"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -16,9 +15,6 @@ db = SQLAlchemy(app)
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(120))
-    #user_id = db.Column(db.Integer)
-    #bodyweight = db.Column(db.Numeric)
-    # jumprope = db.Column(db~.Integer)
     milesran = db.Column(db.Integer)
     pushups = db.Column(db.Integer)
     jumpingjacks = db.Column(db.Integer)
