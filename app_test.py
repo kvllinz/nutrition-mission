f18fcbe6c40b2cccb4742b1fccf08e16d19fe3fb
lindyco <loy80093124@gmail.com>
2021-11-16 17:22:31 +0000

    init server tests

--- app_test.py
+++ app_test.py
@@ -1,12 +1,20 @@
 import unittest
 import flask_testing
 from unittest.mock import patch
-from app import login_post
+from app import login_post 
 
 
 # class loginTest(unittest.TestCase):
 #     def setUp(self) -> None:
 #         return super().setUp()
+
+
+class NutritionMissionTest(unittest.TestCase):
+    def setUp(self):
+        self.mock_db_entries = [ 
+            
+            email=email, name=name, age=age, gender=gender, weight=weight, height=height
+        ]
 
 
 if __name__ == "__main__":
