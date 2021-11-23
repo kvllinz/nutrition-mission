e90bf62dd6c7ef05f19a7f53ce8892893fc21e94
Muhibhotak <72638193+Muhibhotak@users.noreply.github.com>
2021-11-23 12:43:37 -0500

    deleted all changes

--- app.py
+++ app.py
@@ -2,17 +2,20 @@
 App.py
 """
 # pylint: disable=no-member
-# pylint: disable=bare-except
 # from typing import Text
 import os
 import json
 from dotenv import find_dotenv, load_dotenv
-from flask_login import LoginManager, login_manager
+from flask_login import (
+    LoginManager,
+    login_manager,
+    # login_user,
+    current_user,
+)
+
 import flask
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
-from sqlalchemy.sql.expression import null
-from spoonacular import getrecipeswithcalories
 
 load_dotenv(find_dotenv())
 
@@ -40,7 +43,6 @@ class CreateUser(db.Model):
     """
     Model for Users
     """
-
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     email = sqlalchemy.Column(sqlalchemy.String(120), unique=True)
     name = sqlalchemy.Column(sqlalchemy.String(120), unique=True)
@@ -50,32 +52,24 @@ class CreateUser(db.Model):
     height = sqlalchemy.Column(sqlalchemy.String(3))
 
 
+db.drop_all()
+db.create_all()
+
+
 class Workout(db.Model):
-
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    Milesrun = sqlalchemy.Column(sqlalchemy.String(10))
-    pushups = sqlalchemy.Column(sqlalchemy.String(10))
-    jumpingjacks = sqlalchemy.Column(sqlalchemy.String(10))
-    swimming = sqlalchemy.Column(sqlalchemy.String(10))
-    jogging = sqlalchemy.Column(sqlalchemy.String(10))
-    Bicycling = sqlalchemy.Column(sqlalchemy.String(10))
-    ropeclimb = sqlalchemy.Column(sqlalchemy.String(10))
-    toereaches = sqlalchemy.Column(sqlalchemy.String(10))
-    crunches = sqlalchemy.Column(sqlalchemy.String(10))
-
-    # id = db.Column(db.Integer, primary_key=True)
-    # milesran = db.Column(db.Integer)
-    # pushups = db.Column(db.Integer)
-    # jumpingjacks = db.Column(db.Integer)
-    # swimming = db.Column(db.Integer)
-    # jogging = db.Column(db.Integer)
-    # Bicycling = db.Column(db.Integer)
-    # ropeclimb = db.Column(db.Integer)
-    # toereaches = db.Column(db.Integer)
-    # crunches = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    milesran = db.Column(db.Integer)
+    pushups = db.Column(db.Integer)
+    jumpingjacks = db.Column(db.Integer)
+    swimming = db.Column(db.Integer)
+    jogging = db.Column(db.Integer)
+    Bicycling = db.Column(db.Integer)
+    ropeclimb = db.Column(db.Integer)
+    toereaches = db.Column(db.Integer)
+    crunches = db.Column(db.Integer)
 
 
-# db.drop_all()
+db.drop_all()
 db.create_all()
 
 
@@ -85,11 +79,6 @@ def load_user(user_id):
     Load a user
     """
     return CreateUser.query.get(int(user_id))
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return Workout.query.get(int(user_id))
 
 
 @app.route("/", defaults={"path": ""})
@@ -129,30 +118,12 @@ def login_post():
     gender = flask.request.json.get("gender")
     weight = flask.request.json.get("weight")
     height = flask.request.json.get("height")
-    Milesrun = flask.request.json.get("Milesrun")
-    pushups = flask.request.json.get("pushups")
-    jumpingjacks = flask.request.json.get("jumpingjacks")
-    swimming = flask.request.json.get("swimming")
-    jogging = flask.request.json.get("jogging")
-    Bicycling = flask.request.json.get("Bicycling")
-    ropeclimb = flask.request.json.get("ropeclimb")
-    toereaches = flask.request.json.get("toereaches")
-    crunches = flask.request.json.get("crunches")
     checkemail = CreateUser.query.filter_by(email=email).first()
     if checkemail:
         checkemail.age = age
         checkemail.height = height
         checkemail.weight = weight
         checkemail.gender = gender
-        checkemail.Milesrun = Milesrun
-        checkemail.pushups = pushups
-        checkemail.jumpingjacks = jumpingjacks
-        checkemail.swimming = swimming
-        checkemail.jogging = jogging
-        checkemail.Bicycling = Bicycling
-        checkemail.ropeclimb = ropeclimb
-        checkemail.toereaches = toereaches
-        checkemail.crunches = crunches
         db.session.commit()
     else:
         user = CreateUser(
@@ -164,7 +135,7 @@ def login_post():
 
 
 @app.route("/getuserinfo", methods=["POST"])
-def userinfo():
+def userInfo():
     """
     Send UserData if it exists to the frontend
     """
@@ -172,130 +143,36 @@ def userinfo():
         email = flask.request.json.get("email")
         user = CreateUser.query.filter_by(email=email).first()
         cal = usercalories(user.email)
-        recipes = getrecipeswithcalories(cal)
         data = {
             "weight": user.weight,
             "height": user.height,
             "age": user.age,
             "gender": user.gender,
-            "calories": cal,
-            "recipes": recipes,
+            "calories": cal
         }
     except:
-        recipes = getrecipeswithcalories(2000)
         data = {
             "weight": 0,
             "height": 0,
             "age": 0,
             "gender": 0,
-            "calories": 0,
-            "recipes": recipes,
+            "calories": 0
         }
     return flask.jsonify({"data": data})
 
 
-def usercalories(useremail):
+def usercalories(userEmail):
     """
     Get calories needed from the user
     """
-    user = CreateUser.query.filter_by(email=useremail).first()
-    caloriesneeded = (
-        (10 * int(user.weight)) + (6.25 * int(user.height)) - (5 * int(user.age))
-    )
+    user = CreateUser.query.filter_by(email=userEmail).first()
+    caloriesneeded = (10 * int(user.weight)) + \
+        (6.25 * int(user.height)) - (5 * int(user.age))
     if user.gender == "F":
         caloriesneeded -= 161
     elif user.gender == "M":
         caloriesneeded += 5
-    return caloriesneeded
-
-
-@app.route("/login", methods=["POST"])
-def login_posst():
-    """
-    Get username and password from client, check if it is valid and log them in
-    """
-    name = flask.request.json.get("name")
-    email = flask.request.json.get("email")
-    Milesrun = flask.request.json.get("Milesrun")
-    pushups = flask.request.json.get("pushups")
-    jumpingjacks = flask.request.json.get("jumpingjacks")
-    swimming = flask.request.json.get("swimming")
-    jogging = flask.request.json.get("jogging")
-    Bicycling = flask.request.json.get("Bicycling")
-    ropeclimb = flask.request.json.get("ropeclimb")
-    toereaches = flask.request.json.get("toereaches")
-    crunches = flask.request.json.get("crunches")
-
-    checkemail = Workout.query.filter_by(email=email).first()
-    if checkemail:
-        checkemail.Milesrun = Milesrun
-        checkemail.pushups = pushups
-        checkemail.jumpingjacks = jumpingjacks
-        checkemail.swimming = swimming
-        checkemail.jogging = jogging
-        checkemail.Bicycling = Bicycling
-        checkemail.ropeclimb = ropeclimb
-        checkemail.toereaches = toereaches
-        checkemail.crunches = crunches
-
-        db.session.commit()
-    else:
-        user = Workout(
-            email=email, name=name, Milesrun=Milesrun, pushups=pushups, jumpingjacks=jumpingjacks, swimming=swimming, jogging=jogging, Bicycling=Bicycling, ropeclimb=ropeclimb, toereaches=toereaches, crunches=crunches
-        )
-        db.session.add(user)
-        db.session.commit()
-    return flask.jsonify({"loginResponse": "Ok"})
-
-
-@app.route("/Try1", methods=["POST"])
-def userinfoo():
-    """
-    Send UserData if it exists to the frontend
-    """
-    try:
-        email = flask.request.json.get("email")
-        user = CreateUser.query.filter_by(email=email).first()
-        cal = usercalories(user.email)
-        recipes = getrecipeswithcalories(cal)
-        data = {
-            "Milesrun": user.Milesrun,
-            "pushups": user.pushups,
-            "jumpingjacks": user.jumpingjacks,
-            "swimming": user.swimming,
-            "jogging": user.jogging,
-            "Bicycling": user.Bicycling,
-            "ropeclimb": user.ropeclimb,
-            "toereaches": user.toereaches,
-            "crunches": user.crunches,
-        }
-
-    except:
-        recipes = getrecipeswithcalories(2000)
-        data = {
-            "weight": 0,
-            "height": 0,
-            "age": 0,
-            "gender": 0,
-            "calories": 0,
-            "recipes": recipes,
-        }
-    return flask.jsonify({"data": data})
-
-
-def usercalories(useremail):
-    """
-    Get calories needed from the user
-    """
-    user = CreateUser.query.filter_by(email=useremail).first()
-    caloriesneeded = (
-        (10 * int(user.weight)) + (6.25 * int(user.height)) - (5 * int(user.age))
-    )
-    if user.gender == "F":
-        caloriesneeded -= 161
-    elif user.gender == "M":
-        caloriesneeded += 5
-    return caloriesneeded
+    return(caloriesneeded)
 
 
 if __name__ == "__main__":
